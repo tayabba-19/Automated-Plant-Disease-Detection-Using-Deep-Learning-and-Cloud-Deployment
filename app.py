7a06874e4a0fe10a6ac44482d9da4372739ea799
tayabba-19 <tayabbazabeen@gmail.com>
2025-12-31 20:19:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-import pandas as pd
-import streamlit as st
 
-st.title("Automated plant disease detection using deep learning and cloud deployment")
-
-ipynb_url = "https://raw.githubusercontent.com/tayabba-19/Automated-Plant-Disease-Detection-Using-Deep-Learning-and-Cloud-Deployment/refs/heads/main/Final_Project.ipynb"
-df = pd.read_ipynb(ipynb_url)
-
-st.success("Dataset loaded successfully!")
-st.dataframe(df.head())
         
 
