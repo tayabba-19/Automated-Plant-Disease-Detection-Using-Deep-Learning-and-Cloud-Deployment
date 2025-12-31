7659598c9b97e903f3f406bec62194bf835be930
tayabba-19 <tayabbazabeen@gmail.com>
2025-12-31 20:20:16 +0530

    Update gradcam.py

--- gradcam.py
+++ gradcam.py
@@ -1,1 +1,23 @@
+import tensorflow as tf
+import numpy as np
+import cv2
 
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name):
+    grad_model = tf.keras.models.Model(
+        [model.inputs],
+        [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(img_array)
+        loss = predictions[:, tf.argmax(predictions[0])]
+
+    grads = tape.gradient(loss, conv_outputs)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    conv_outputs = conv_outputs[0]
+    heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    heatmap = np.maximum(heatmap, 0) / np.max(heatmap)
+    return heatmap
